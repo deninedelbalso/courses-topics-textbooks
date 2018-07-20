32da74c842db1b5be82c36fcdfcbb13401afacd1
deninedelbalso <ddelbals@kent.edu>
2018-07-19 22:52:46 -0400

    commit

---
+++ src/main/resources/static/js/courses-ajax-example.js
@@ -0,0 +1,36 @@
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+                console.log=JSON.parse(xhr.response);
+                const res = JSON.parse(xhr.response)
+                const container = document.querySelector('.container')
+
+                console.log({res})
+
+
+                res.forEach{function(course){
+                	const courseItem= document.createElement('div')
+
+                	const name = document.createElement('h2')
+                	name.innerText = course.name;
+
+                	const descripton = document.createElement('p')
+                	description.innerText = course.description;
+
+                	const topicUrls= [];
+                	course.topicUrls.forEach(topicUrl =>{
+                		const topicUrlElement = document.createElement('li')
+                		topicUrlElement.innerHtml= 'course topics: <a href="${topicUrl}">${topicUrl}</a>`
+ 						topicUrls.push(topicUrlElement)
+                	container.appendChild(courseItem)
+                	courseItem.appendChild(name)
+                	courseItem.appendChild(description)
+
+                	topicUrl.forEach(topicUrl => courseItem.appendChild(topicUrl))
+
+                });
+
+            }
+        }
+        xhr.open('GET', 'http://localhost:8080/courses', true)
+        xhr.send()
